5a1113635a7daf01dbf3af32b06593fbbf393a8a
Constance  Nwaigwe <cnwaigwe@perscholas.org>
2024-01-29 19:58:44 -0500

    practice problems for array and objects

---
+++ arrays.js
@@ -0,0 +1,67 @@
+let characters = [
+  {
+    firstname: "Rick",
+    lastname: "Sanchez",
+    email: "pickle_rick@gmail.com",
+    likes: ["Szechuan Sauce", "Beth", "Morty", "Summer"],
+    dislikes: ["Goverment", "Jerry"],
+  },
+  {
+    firstname: "Morty",
+    lastname: "Smith",
+    email: "morty@gmail.com",
+    likes: ["Rick", "Jessica"],
+    dislikes: ["Being bullied"],
+  },
+  {
+    firstname: "Summer",
+    lastname: "Smith",
+    email: "SSSummer@gmail.com",
+    likes: ["Social media", "Being popular"],
+    dislikes: ["Not being popular"],
+  },
+  {
+    firstname: "Beth",
+    lastname: "Smith",
+    email: "BethSmith@gmail.com",
+    likes: ["Beth Clone", "Morty", "Summer"],
+    dislikes: ["Unknown"],
+  },
+  {
+    firstname: "Beth",
+    lastname: "Clone",
+    email: "notaclone@gmail.com",
+    likes: ["Beth Smith", "Morty", "Summer"],
+    dislikes: ["Unknown"],
+  },
+  {
+    firstname: "Jerry",
+    lastname: "Smith",
+    email: "imjerry@gmail.com",
+    likes: ["Beth Smith", "Morty", "Summer"],
+    dislikes: ["Rick"],
+  },
+];
+
+let sideCharacters = [
+  ["Mr.P", 1],
+  ["Birdperson", 2],
+  ["Gearhead", 2],
+  ["Dr.Wong", 3],
+];
+
+// in characters variable print out every charater with the last name smith
+
+// in characters variable print out every charater named beth
+
+// in characters variable print out rick's email
+
+// in sidecharacters print out the characters with a number 2
+
+// in sidecharacters print out Dr.Wong only, not the number attached
+
+// print out rick's first like in the likes array
+
+// print out rick's last dislike in the dislake array
+
+// print all of jerry's likes
